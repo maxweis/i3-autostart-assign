ae0348eb84967b155dbc7ee8796394ab037bbd0c
Max Weis <maxrobweis@gmail.com>
2020-04-16 02:44:25 -0500

    support for workspace names and app names with spaces, add comments

--- i3-autostart-assign.sh
+++ i3-autostart-assign.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
-APP_LAUNCH_WAIT=5 #time to wait for apps to launch, may need to be changed
+#time to wait for apps to launch, may need to be changed
+APP_LAUNCH_WAIT=5
 
 CONFIG=$HOME/.config/i3/config #config to modify
-CONFIG_BACKUP=${CONFIG}_startup_backup #place to backup unmodified config 
+CONFIG_BACKUP=${CONFIG}_startup_backup #backup unmodified config location
 
 APPS=(firefox urxvt) #binaries to open
-NAMES=(firefox urxvt) #corresponding names of apps to open
-WORKSPACES=(1, \$ws2) #name of workspaces to open apps in 
-CRITERIA=(class class) #criteria to base name on, choose from class, id, instance, title
+NAMES=("firefox" "urxvt") #corresponding names of apps to open
+WORKSPACES=("1" "EDIT") #name of workspaces to open apps in 
+CRITERIA=(class title)  #criterion for comparing name, used by i3
+                        #choose from class, id, instance, title
+                        #usually use class and the value from xprop
 
-#backup config
+
 cp $CONFIG $CONFIG_BACKUP
-
-append_config() {
-    echo "for_window [$1=\"$2\"] move to workspace $3" >> $CONFIG
-}
 
 len=${#APPS[@]} #number of apps
 for ((i=0; i<=$(expr $len - 1); i++)) do #append config for all apps
-    append_config ${CRITERIA[i]} ${NAMES[i]} ${WORKSPACES[i]}
+    APP=${APPS[i]}
+    NAME=${NAMES[i]}
+    WORKSPACE=${WORKSPACES[i]}
+    CRITERION=${CRITERIA[i]}
+    echo "for_window [$CRITERION=\"$NAME\"] move to workspace \"$WORKSPACE\"" >> $CONFIG
 done
 
 #sync writes to config to disk, restart i3 with modified config
